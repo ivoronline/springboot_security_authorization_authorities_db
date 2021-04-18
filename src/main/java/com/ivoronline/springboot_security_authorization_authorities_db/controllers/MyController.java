7dc953ec8cc7f8ecf506b8db53ecf8c4988fee4c
igoreta <ivoronline@gmail.com>
2021-04-18 15:35:07 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_authorization_authorities_db/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_authorization_authorities_db/controllers/MyController.java
@@ -2,34 +2,29 @@ package com.ivoronline.springboot_security_authorization_authorities_db.controll
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
 
-  @ResponseBody
   @PreAuthorize("hasAuthority('book.create')")
   @RequestMapping("/CreateBook")
   public String createBook() {
     return "Only ADMIN can create Book";
   }
 
-  @ResponseBody
   @PreAuthorize("hasAuthority('book.read')")
   @RequestMapping("/ReadBook")
   public String readBook() {
     return "ADMIN and USER can read Book";
   }
 
-  @ResponseBody
   @PreAuthorize("hasAuthority('book.update')")
   @RequestMapping("/UpdateBook")
   public String updateBook() {
     return "Only ADMIN can update Book";
   }
 
-  @ResponseBody
   @PreAuthorize("hasAuthority('book.delete')")
   @RequestMapping("/DeleteBook")
   public String deleteBook() {
